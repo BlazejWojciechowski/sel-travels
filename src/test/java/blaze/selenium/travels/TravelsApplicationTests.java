5c4b7b4e5dcc27d8ddb9291a47709a8f09e31e4e
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-04-25 11:41:27 +0200

    initial commit

---
+++ src/test/java/blaze/selenium/travels/TravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package blaze.selenium.travels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
