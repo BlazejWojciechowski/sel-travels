ab35f3c82444c8fefd69e24c39d3896d74f409da
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-04-26 15:05:15 +0200

    added empty Sign Up form test

---
+++ src/test/java/blaze/selenium/travels/search/InvalidSignUpTest.java
@@ -0,0 +1,55 @@
+package blaze.selenium.travels.search;
+
+import blaze.selenium.travels.service.FactoryDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class InvalidSignUpTest {
+    @Test
+    public void singUp() {
+        WebDriver driver = FactoryDriver.getDriver("firefox");
+
+        Duration time = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, time);
+
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+        driver.findElements(By.xpath("//li[@id='li_myaccount']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']"))
+                .get(1)
+                .click();
+
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        List<String> errors = driver.findElements(By.xpath("//div[@class='alert alert-danger']//p"))
+                .stream()
+                .map(WebElement::getText)
+                .toList();
+
+        errors.forEach(System.out::println);
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(errors.contains("The Email field is required."));
+        softAssert.assertTrue(errors.contains("The Password field is required."));
+        softAssert.assertTrue(errors.contains("The Password field is required."));
+        softAssert.assertTrue(errors.contains("The First name field is required."));
+        softAssert.assertTrue(errors.contains("The Last Name field is required."));
+        softAssert.assertAll();
+    }
+}
