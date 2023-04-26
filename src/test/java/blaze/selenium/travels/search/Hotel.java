a00400824c393878703c26f6622c11a0c62e0b44
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-04-26 11:16:51 +0200

    added few more test methods to Hotel class

--- src/test/java/blaze/selenium/travels/search/Hotel.java
+++ src/test/java/blaze/selenium/travels/search/Hotel.java
@@ -3,12 +3,15 @@ package blaze.selenium.travels.search;
 import blaze.selenium.travels.service.FactoryDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Hotel {
     @Test
@@ -25,5 +28,27 @@ public class Hotel {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
                 "//span[@class='select2-match' and text()='Dubai']")));
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+        driver.findElement(By.name("checkin")).sendKeys("18/04/2024");
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.xpath("//button[(text()= ' Search')]")).click();
+
+        List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class, 'list_title')]//b"))
+                .stream()
+                .map(element -> element.getAttribute("textContent")) // .map(element -> element.getText) //(WebElement::getText)
+                .toList();
+
+        Assert.assertEquals("Jumeirah Beach Hotel", hotelNames.get(0));
+        Assert.assertEquals("Oasis Beach Tower", hotelNames.get(1));
+        Assert.assertEquals("Rose Rayhaan Rotana", hotelNames.get(2));
+        Assert.assertEquals("Hyatt Regency Perth", hotelNames.get(3));
     }
 }
