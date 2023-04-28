054c0ee4eb105ff2224512e83ce6f4f0f4630906
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-04-28 12:40:20 +0200

    created LoggedUserPage as a next POP

---
+++ src/test/java/blaze/selenium/travels/pages/LoggedUserPage.java
@@ -0,0 +1,22 @@
+package blaze.selenium.travels.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoggedUserPage {
+    @FindBy(xpath = "//h3[@class='RTL']")
+    private WebElement heading;
+    private WebDriver driver;
+    private WebDriverWait wait;
+    public LoggedUserPage(WebDriver driver, WebDriverWait wait) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+        this.wait = wait;
+    }
+    public String getHeadingText() {
+        return heading.getText();
+    }
+}
